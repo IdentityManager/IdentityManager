4338e3a2f510f812515fb70805a57e99d162972d
Brock Allen <brockallen@gmail.com>
2014-07-17 01:03:36 -0400

    add in-memory versions of stores

---
+++ source/AspNetIdentity.Tests/InMemoryRole.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetIdentity.Tests
+{
+    public class InMemoryRole : IRole
+    {
+        public InMemoryRole(string roleName)
+        {
+            Id = Guid.NewGuid().ToString();
+            Name = roleName;
+        }
+
+        public virtual string Id { get; set; }
+        public virtual string Name { get; set; }
+    }
+
+}
---
+++ source/AspNetIdentity.Tests/LoginComparer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetIdentity.Tests
+{
+    public class LoginComparer : IEqualityComparer<UserLoginInfo>
+    {
+        public bool Equals(UserLoginInfo x, UserLoginInfo y)
+        {
+            return x.LoginProvider == y.LoginProvider && x.ProviderKey == y.ProviderKey;
+        }
+
+
+        public int GetHashCode(UserLoginInfo obj)
+        {
+            return (obj.ProviderKey + "--" + obj.LoginProvider).GetHashCode();
+        }
+    }
+
+}
