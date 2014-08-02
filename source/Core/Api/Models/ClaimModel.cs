8c48d94b4d1b52259d5a7de2c0f933f5662a588a
Brock Allen <brockallen@gmail.com>
2014-08-02 12:40:07 -0400

    validation for url characters in claim type/value

--- source/Core/Api/Models/ClaimModel.cs
+++ source/Core/Api/Models/ClaimModel.cs
@@ -3,6 +3,7 @@
  * see license
  */
 using System.ComponentModel.DataAnnotations;
+using Thinktecture.IdentityManager.Core.Api.Validation;
 using Thinktecture.IdentityManager.Core.Resources;
 
 namespace Thinktecture.IdentityManager.Api.Models
@@ -10,8 +11,10 @@ namespace Thinktecture.IdentityManager.Api.Models
     public class ClaimModel
     {
         [Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = "ClaimTypeRequired")]
+        [NoUrlCharactersAttribute]
         public string Type { get; set; }
         [Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = "ClaimValueRequired")]
+        [NoUrlCharactersAttribute]
         public string Value { get; set; }
     }
 }
