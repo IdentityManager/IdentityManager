c58ee10b13da38509eea579587ea20fd3a2e663d
Brock Allen <brockallen@gmail.com>
2014-08-02 14:50:45 -0400

    set security mode in startup

--- source/Host/Startup.cs
+++ source/Host/Startup.cs
@@ -17,6 +17,7 @@ namespace Thinktecture.IdentityManager.Host
             {
                 IdentityManagerFactory = () => svc,
                 DisableUserInterface = false,
+                SecurityMode = SecurityMode.Local
             });
         }
     }
