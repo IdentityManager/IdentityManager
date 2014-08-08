c87bee73a9815563afdeb81779e62fe7fe9c135c
Brock Allen <brockallen@gmail.com>
2014-08-08 18:46:27 -0400

    default user metadata properties to empty collection

--- source/Core/Core/IdentityManagerMetadata.cs
+++ source/Core/Core/IdentityManagerMetadata.cs
@@ -12,7 +12,10 @@ namespace Thinktecture.IdentityManager.Core
     {
         public IdentityManagerMetadata()
         {
-            this.UserMetadata = new UserMetadata();
+            this.UserMetadata = new UserMetadata()
+            {
+                Properties = Enumerable.Empty<PropertyMetadata>()
+            };
         }
 
         public UserMetadata UserMetadata { get; set; }
@@ -21,26 +24,6 @@ namespace Thinktecture.IdentityManager.Core
         {
             if (UserMetadata == null) throw new InvalidOperationException("UserMetadata not assigned.");
             UserMetadata.Validate();
-        }
-    }
-
-    public class UserMetadata
-    {
-        public UserMetadata()
-        {
-            this.Properties = new HashSet<PropertyMetadata>();
-        }
-
-        public bool SupportsCreate { get; set; }
-        public bool SupportsDelete { get; set; }
-
-        public bool SupportsClaims { get; set; }
-        public ICollection<PropertyMetadata> Properties { get; set; }
-
-        internal void Validate()
-        {
-            if (Properties == null) throw new InvalidOperationException("Properties not assigned.");
-            foreach (var prop in Properties) prop.Validate();
         }
     }
 }
---
+++ source/Core/Core/UserMetadata.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Copyright (c) Dominick Baier, Brock Allen.  All rights reserved.
+ * see license
+ */
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Thinktecture.IdentityManager.Core
+{
+    public class UserMetadata
+    {
+        public bool SupportsCreate { get; set; }
+        public bool SupportsDelete { get; set; }
+
+        public bool SupportsClaims { get; set; }
+        public IEnumerable<PropertyMetadata> Properties { get; set; }
+
+        internal void Validate()
+        {
+            if (Properties == null) throw new InvalidOperationException("Properties not assigned.");
+            foreach (var prop in Properties) prop.Validate();
+        }
+    }
+}
