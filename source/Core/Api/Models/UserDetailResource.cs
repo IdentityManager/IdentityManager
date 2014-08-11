e39bc865585fed8d6306f124b0ad4d0451559e43
Brock Allen <brockallen@gmail.com>
2014-08-11 16:52:14 -0400

    license

--- source/Core/Api/Models/UserDetailResource.cs
+++ source/Core/Api/Models/UserDetailResource.cs
@@ -1,4 +1,8 @@
-﻿using Newtonsoft.Json.Linq;
+﻿/*
+ * Copyright (c) Dominick Baier, Brock Allen.  All rights reserved.
+ * see license
+ */
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
