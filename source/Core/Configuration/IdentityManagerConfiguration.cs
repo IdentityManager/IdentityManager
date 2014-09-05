857118773dea9bd971caec71389a2b12673842a2
Brock Allen <brockallen@gmail.com>
2014-09-04 20:33:28 -0500

    split files

--- source/Core/Configuration/IdentityManagerConfiguration.cs
+++ source/Core/Configuration/IdentityManagerConfiguration.cs
@@ -7,36 +7,6 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace Thinktecture.IdentityManager
 {
-    public class OAuth2Configuration
-    {
-        public OAuth2Configuration()
-        {
-            Scope = Constants.IdMgrScope;
-        }
-
-        public string AuthorizationUrl { get; set; }
-        public string Scope { get; set; }
-        public string ClientId { get; set; }
-
-        public string Audience { get; set; }
-        public string Issuer { get; set; }
-        
-        public string SigningKey { get; set; }
-        public X509Certificate2 SigningCert { get; set; }
-
-        internal void Validate()
-        {
-            if (String.IsNullOrWhiteSpace(AuthorizationUrl)) throw new InvalidOperationException("OAuth2Configuration : AuthorizationUrl not configured");
-            if (String.IsNullOrWhiteSpace(Scope)) throw new InvalidOperationException("OAuth2Configuration : Scope not configured");
-            if (String.IsNullOrWhiteSpace(ClientId)) throw new InvalidOperationException("OAuth2Configuration : ClientId not configured");
-
-            if (String.IsNullOrWhiteSpace(Audience)) throw new InvalidOperationException("OAuth2Configuration : Audience not configured");
-            if (String.IsNullOrWhiteSpace(Issuer)) throw new InvalidOperationException("OAuth2Configuration : Issuer not configured");
-
-            if (String.IsNullOrWhiteSpace(SigningKey) && SigningCert == null) throw new InvalidOperationException("OAuth2Configuration : Signing key not configured");
-        }
-    }
-
     public class IdentityManagerConfiguration
     {
         public IdentityManagerConfiguration()
